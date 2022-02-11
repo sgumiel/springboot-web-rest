c74b3e385dcb20fefd2589c17831ebf00e490e77
Sergio Gumiel <sergio.gumiel@kairosds.com>
2022-02-11 08:49:29 +0100

    First commit

---
+++ src/test/java/com/kairosds/cursospb2/webrest/WebRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kairosds.cursospb2.webrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
